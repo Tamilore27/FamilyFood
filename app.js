8ab8ef3bd0852e6e8182a03a4bc7a29df6837bd4
Tamilore27 <97904480+Tamilore27@users.noreply.github.com>
2026-02-19 13:31:22 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+<script src="https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js"></script>
+
+
 /************************************************************
  LOAD EXCEL MEALS
 *************************************************************/
